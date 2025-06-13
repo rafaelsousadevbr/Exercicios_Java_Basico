340a3500c4355894acbc420a44634e7f914e30e8
rafaelsousadevbr <rafael@sousalima.dev.br>
2025-06-12 21:52:42 -0300

    Inicializado repositório dos exercícios do bootcamp e resolução do primeiro exercício

---
+++ src/Exercicio01.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Exercicio01 {
+    public static void main(String[] args) throws Exception {
+        Scanner scanner = new Scanner(System.in);
+        
+        System.out.println("Digite seu nome:");
+        String nome = scanner.nextLine();
+        
+        System.out.println("Digite seu ano de nascimento:");
+        int anoNascimento = scanner.nextInt();
+
+        int anoAtual = java.time.Year.now().getValue();
+        int idade = anoAtual - anoNascimento;
+        System.out.println("Olá " + nome + ", você tem " + idade + " anos.");
+    }
+}
