e95fbac91be73432efdba4a5ada8e99269b23936
rafaelsousadevbr <rafael@sousalima.dev.br>
2025-07-07 13:26:48 -0300

    Adicionada a implementação da classe PetMachine com métodos para gerenciar banho e adição de água e shampoo.

---
+++ src/PetShop/PetMachine.java
@@ -0,0 +1,35 @@
+package PetShop;
+
+public class PetMachine {
+    private boolean clean;
+    private int water;
+    private int shampoo;
+    private Pet pet;
+
+    public void takeAShower(){
+        if(this.pet == null) {
+            System.out.println("Place the pet in the machine to start the bath.");
+            return;
+        }
+
+        pet.setClean(true);
+        System.out.println("The pet " + pet.getName() + " is clean.");
+    }
+
+    public void addWater(){
+        if(water == 10){
+            System.out.println("A capacidade de água da máquina está no máximo");
+            return;
+        }
+        water += 2;
+    }
+
+    public void addShampoo(){
+        if(shampoo == 10){
+            System.out.println("The machine's shampoo capacity is at maximum.");
+            return;
+        }
+        shampoo += 2;
+    }
+
+}
